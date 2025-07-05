5b0fdb7ffde671f6c79602814be83c27847b1440
0xWulf <0xwulf@proton.me>
2025-07-05 16:03:55 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter"; // ✅ import Wouter Router
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router> {/* ✅ Wrap App in Wouter Router */}
+    <App />
+  </Router>
+);
